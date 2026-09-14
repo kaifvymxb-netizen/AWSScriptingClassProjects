ee82f2ae7725e74c021e580ac6ec2ccfe369950d
Frasier Van <KFRASIERVAN@madisoncollege.edu>
2026-09-14 16:29:59 -0500

    Update project files

---
+++ Week3/builds3website.py
@@ -0,0 +1,43 @@
+import boto3, json
+
+# Building a static website in S3 with Python and Boto3
+s3client = boto3.client('s3')
+myBucketName = 'my-static-website-bucket-1928'
+s3client.create_bucket(Bucket=myBucketName)
+s3client.delete_public_access_block(Bucket=myBucketName)
+bucket_policy = { 
+
+    'Version': '2012-10-17', 
+
+    'Statement': [{ 
+
+        'Sid': 'AddPerm', 
+
+        'Effect': 'Allow', 
+
+        'Principal': '*', 
+
+        'Action': ['s3:GetObject'], 
+
+        'Resource': "arn:aws:s3:::%s/*" % myBucketName 
+
+     }] 
+
+}
+
+s3client.put_bucket_policy(Bucket=myBucketName, Policy=json.dumps(bucket_policy))
+put_bucket_response = s3client.put_bucket_website( 
+    Bucket=myBucketName, 
+    WebsiteConfiguration={ 
+     'ErrorDocument': {'Key': 'error.html'}, 
+    'IndexDocument': {'Suffix': 'index.html'}, 
+    } 
+) 
+file = "index.html"
+body = open(file, 'rb').read()
+s3client.put_object(
+    Body=body, 
+    Bucket=myBucketName, 
+    Key=file, 
+    ContentType='text/html' )
+file.close()
